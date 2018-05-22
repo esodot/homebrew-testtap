39698ba1d235a2c22ef824763f094e6c91aa0173
Alexander Gerk <alexandergerk@Alexanders-MacBook-Pro.local>
2018-05-22 15:25:08 +0200

    add formula

---
+++ testscript.rb
@@ -0,0 +1,11 @@
+class Testscript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/esodot/testscript/archive/v1.0.0.tar.gz"
+  sha256 "a070764645880d21056ead6da6e1e347efed364474379835f2caf90f0507b582"
+
+  def install
+  	bin.install "testscript"
+  end
+
+end
