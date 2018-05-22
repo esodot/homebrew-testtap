e86774371611f4a09e665bd06666e4f41350815a
Alexander Gerk <alexandergerk@Alexanders-MacBook-Pro.local>
2018-05-22 18:03:58 +0200

    Update back to v1.0.0

--- testscript.rb
+++ testscript.rb
@@ -1,7 +1,7 @@
 class Testscript < Formula
   desc ""
   homepage ""
-  url "https://github.com/esodot/testscript/archive/v1.0.1.tar.gz"
+  url "https://github.com/esodot/testscript/archive/v1.0.0.tar.gz"
   sha256 "a070764645880d21056ead6da6e1e347efed364474379835f2caf90f0507b582"
 
   def install
