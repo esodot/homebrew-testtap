1eb5f15bd99c509f81c6a8606c00d8852f70369b
Alexander Gerk <alexandergerk@Alexanders-MacBook-Pro.local>
2018-05-24 11:44:06 +0200

    V1.0.1

--- testscript.rb
+++ testscript.rb
@@ -1,11 +1,9 @@
 class Testscript < Formula
   desc ""
   homepage ""
-  url "https://github.com/esodot/testscript/archive/v1.0.0.tar.gz"
-  sha256 "a070764645880d21056ead6da6e1e347efed364474379835f2caf90f0507b582"
-
+  url "https://github.com/esodot/testscript/archive/v1.0.1.tar.gz"
+  sha256 "7730ba9f250a4b5b9e61d1718e88ea3af87fba4c8472188f56de73c1e3929d2a"
   def install
-  	bin.install "testscript"
+	bin.install "testscript"
   end
-
 end
